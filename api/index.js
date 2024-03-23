c3b56c99790bc5bbfb92c8b2f34e3347697a9c7e
MXC2002 <manminh.2510@gmail.com>
2024-03-23 18:14:43 +0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000!!');
-})
+
+mongoose.connect(process.env.MONGODB)
+    .then(() => {
+        app.listen(5000, () => {
+            console.log('Connected to MongoDB & Server running on port 5000!!');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
