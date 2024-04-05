f40ddc4648b0f7e68d182f62351f103c797c8747
MXC2002 <manminh.2510@gmail.com>
2024-04-05 18:22:42 +0700

    complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -18,7 +18,7 @@ export default function DashSidebar() {
       <Sidebar.Items >
         <Sidebar.ItemGroup>
           <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"Người dùng"} labelColor='dark'>
+            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"Người dùng"} labelColor='dark' as='div'>
               Hồ sơ
             </Sidebar.Item>
           </Link>
