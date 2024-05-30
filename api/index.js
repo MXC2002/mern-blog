6b55a9b5809c107fc8bee6cf5d77a275a32f75e5
MXC2002 <manminh.2510@gmail.com>
2024-05-30 21:20:29 +0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import favoriteRoutes from './routes/favorite.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -20,6 +21,8 @@ mongoose.connect(process.env.MONGODB)
     .catch((err) => {
         console.log(err);
     })
+
+    const __dirname = path.resolve()
 
 
 const app = express();
@@ -36,6 +39,11 @@ app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
 app.use('/api/favorite', favoriteRoutes)
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
